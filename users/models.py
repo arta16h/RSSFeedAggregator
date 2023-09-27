843f3fc81e6247ac93a4bde4dcd3b665cfca3814
arta16h <sevda.hayati2015@gmail.com>
2023-09-27 11:00:59 +0330

    Chore: user manager- create user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 # Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, phone, password, **other_fields):
+        if not phone:
+            raise ValueError("The Phone Field Can Not Be Empty!")
+
+        user = self.model(phone=phone, **other_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
