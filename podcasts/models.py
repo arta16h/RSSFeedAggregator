4e7a096e9794562021aec694853ab8bb315d8344
arta16h <sevda.hayati2015@gmail.com>
2023-09-27 11:36:52 +0330

    Chore: owner model

--- podcasts/models.py
+++ podcasts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Owner:
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
