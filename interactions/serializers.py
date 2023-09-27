6ded256bb63b0c3dbb67711c7ffe84e33d137e21
arta16h <sevda.hayati2015@gmail.com>
2023-09-28 02:11:05 +0330

    Chore: like serializer

--- interactions/serializers.py
+++ interactions/serializers.py
@@ -1,2 +1,12 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Like, Comment, Subscribe
+
+class LikeSerializer(ModelSerializer):
+    class META:
+        model = Like
+        fields = [
+            "user",
+            "episode",
+            "created_at",
+            "updated_at",
+        ]
