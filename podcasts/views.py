f3271e409b9673675a158f4923094fefba42ad21
arta16h <sevda.hayati2015@gmail.com>
2023-09-27 13:06:20 +0330

    Chore: episode list view

--- podcasts/views.py
+++ podcasts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Podcast, Episode
+from .serializers import PodcastSerializer, EpisodeSerializer
 
 # Create your views here.
+
+class EpisodeListView():
+
+    def get(self, request):
+        queryset = Episode.objects.all()
+        serializer_data = EpisodeSerializer(queryset, many=True)
+        return Response(serializer_data.data, status=status.HTTP_200_OK)
