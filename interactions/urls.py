97bbd2f25371e5fa532f12ffe955f2c889a39f1f
arta16h <sevda.hayati2015@gmail.com>
2023-09-28 03:25:36 +0330

    Chore: interactions urls

--- interactions/urls.py
+++ interactions/urls.py
@@ -2,3 +2,9 @@ from django.urls import path
 from .views import LikeAPIView, CommentAPIView, SubscribeView, PlaylistAPIView
 
 
+urlpatterns = [
+    path("like/", LikeAPIView.as_view(), name="like"),
+    path("comment/", CommentAPIView.as_view(), name="comment"),
+    path("subscribe/", SubscribeView.as_view(), name="subscribe"),
+    path("playlist/", PlaylistAPIView.as_view(), name="playlist"),
+]
