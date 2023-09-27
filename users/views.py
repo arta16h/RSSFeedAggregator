464c931028264d28e316207f33010a44cffc0a7f
arta16h <sevda.hayati2015@gmail.com>
2023-09-28 01:31:54 +0330

    Chore: register api view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import Response, APIView
+from rest_framework import status
+
+from .models import User
+from .serializers import UserSerializer
 
 # Create your views here.
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
