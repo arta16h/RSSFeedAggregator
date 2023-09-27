3743cd14baaff49e1df602109c86c9a162fc47c9
arta16h <sevda.hayati2015@gmail.com>
2023-09-28 02:04:26 +0330

    Chore: subscribe model

--- interactions/models.py
+++ interactions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+from users.models import User
+from podcasts.models import Podcast, Episode
+
 
 # Create your models here.
+
+class Subscribe:
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user
+
