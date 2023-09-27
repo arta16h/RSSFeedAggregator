eee32e25284c8ef6483eeb01b7fb92d72f43c5e3
arta16h <sevda.hayati2015@gmail.com>
2023-09-27 12:09:45 +0330

    Chore: podcast serializer

--- podcasts/serializers.py
+++ podcasts/serializers.py
@@ -1,3 +1,23 @@
 from rest_framework import serializers
 from .models import Podcast, Episode
 
+
+class PodcastSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Podcast
+        fields = [
+            "title",
+            "description",
+            "category",
+            "author",
+            "rssOwner",
+            "websiteUrl",
+            "isExplicitContent",
+            "copyright",
+            "language",
+            "contentType",
+            "pubDate",
+            "imageUrl",
+            "subtitle",
+            "keywords",
+        ]
