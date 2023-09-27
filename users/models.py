733bc33bd1c3939c1608338a07883bd6488841b7
arta16h <sevda.hayati2015@gmail.com>
2023-09-27 11:01:56 +0330

    Chore: user manager- create superuser

--- users/models.py
+++ users/models.py
@@ -13,3 +13,13 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+    def create_superuser(self, phone, password=None, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(phone, password, **other_fields)
