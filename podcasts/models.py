a025d270a8a827ab4d2f0191dd25e42066cfde86
arta16h <sevda.hayati2015@gmail.com>
2023-09-27 11:38:42 +0330

    Chore: category model

--- podcasts/models.py
+++ podcasts/models.py
@@ -11,3 +11,11 @@ class Owner:
     def __str__(self):
         return self.name
     
+
+class Category:
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
