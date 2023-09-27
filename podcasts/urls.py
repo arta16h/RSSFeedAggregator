f08981d03caf5b6f4b3c8eb25357fa826c38421d
arta16h <sevda.hayati2015@gmail.com>
2023-09-27 23:26:40 +0330

    Chore: add podcast urls

--- podcasts/urls.py
+++ podcasts/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
 from .views import PodcastListView, PodcastDetailView, EpisodeListView, EpisodeDetailView
 
+urlpatterns = [
+    path("podcasts/", PodcastListView.as_view(), name="podcast-list-create"),
+    path("podcasts/<int:pk>/", PodcastDetailView.as_view(), name="podcast-detail"),
+    path("podcasts/<int:pk>/episodes/", EpisodeListView.as_view(), name="podcast-episodes-list-create"),
+    path("episodes/<int:pk>/", EpisodeDetailView.as_view(), name="episode-detail"),
+]
