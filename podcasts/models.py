4ff6846e69c55f0a4bc685ca5d7d48627b1b1f05
arta16h <sevda.hayati2015@gmail.com>
2023-09-28 06:04:47 +0330

    Fix: model inheritance

--- podcasts/models.py
+++ podcasts/models.py
@@ -12,7 +12,7 @@ class Owner:
         return self.name
     
 
-class Category:
+class Category(models.Model):
     name = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
