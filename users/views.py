eb5f300af5f0d7e6952ea2d9b1a4b37dcbcb5198
arta16h <sevda.hayati2015@gmail.com>
2023-09-28 04:04:56 +0330

    Chore: verify otp api view

--- users/views.py
+++ users/views.py
@@ -22,9 +22,19 @@ class SendOTPAPIView(APIView):
         serializer=LoginSerializer(data=request.data, context={"request":request})
         if serializer.is_valid(raise_exception=True):
             serializer.create_otp(request, serializer.data["phone"])
-            return Response (data={"message":"200"})
+            return Response (data={"message":"succeeded"})
         
 
+class VerifyOTPAPIView(APIView):
+    def post(self, request):
+        serliazer=LoginOTPSerializer(data=request.data, context={"request":request})
+        if serliazer.is_valid(raise_exception=True):
+            user=User.objects.get(phone=request.session.get("phone"))
+            access_token=user.get_access_token()
+            refresh_token=user.get_refresh_token()
+            return Response(data={"message":"succeeded", "AT":access_token, "RT":refresh_token})
+
+        
 class LoginAPIView(APIView):
     def post(self, request):
         password = request.data["password"]
