9e41b27b2c60c0ec7853d6d0e0b9db23bb41f20f
arta16h <sevda.hayati2015@gmail.com>
2023-09-28 04:08:09 +0330

    Chore: send otp & verify url

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import RegisterAPIView, LoginAPIView, LogoutAPIView
+from .views import RegisterAPIView, LoginAPIView, LogoutAPIView, SendOTPAPIView, VerifyOTPAPIView
 
 urlpatterns = [
     path("register", RegisterAPIView.as_view(), name="register"),
     path("login", LoginAPIView.as_view(), name="login"),
     path("logout", LogoutAPIView.as_view(), name="logout"),
+    path("sendotp/",SendOTPAPIView.as_view(), name="sendotp"),
+    path("verify/", VerifyOTPAPIView.as_view(), name="verify"),
 ]
