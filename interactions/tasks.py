ccdf4daa61b6c88e30fe2b0b257dc12c3011c43b
arta16h <sevda.hayati2015@gmail.com>
2023-09-29 08:49:42 +0330

    Chore: create comment task

--- interactions/tasks.py
+++ interactions/tasks.py
@@ -1,5 +1,5 @@
 from celery import shared_task, Task
-from .models import Comment
+from .models import Comment, Episode
 
 
 class BaseTask(Task):
@@ -8,3 +8,7 @@ class BaseTask(Task):
     retry_backoff = True
     retry_jitter = False
 
+@shared_task(base=BaseTask, task_time_limit=120)
+def create_comment(content, episode_id, user_id):
+    Comment.objects.create(content=content, object_id=episode_id, user_id=user_id)
+    return f"New comment submitted by {user_id}"
