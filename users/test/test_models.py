9ded09480731017b85294021fdbaf80267f66433
arta16h <sevda.hayati2015@gmail.com>
2023-09-29 08:12:05 +0330

    Chore: setup

--- users/test/test_models.py
+++ users/test/test_models.py
@@ -2,3 +2,9 @@ from django.test import TestCase
 from django.db.utils import IntegrityError
 from ..models import User
 
+
+class UserModelTest(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(username='testuser', email='test@example.com', password='testpassword')
