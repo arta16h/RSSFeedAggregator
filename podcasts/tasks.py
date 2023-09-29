ba395d6db88848e830075842b869f5f02a2b7d98
arta16h <sevda.hayati2015@gmail.com>
2023-09-29 07:10:12 +0330

    Chore: podcast handler - on failure

--- podcasts/tasks.py
+++ podcasts/tasks.py
@@ -1,5 +1,7 @@
 import logging
 from celery import shared_task
+from celery.worker.request import Request
+from celery.exceptions import Retry
 from .models import Podcast
 
 
@@ -7,3 +9,16 @@ logger = logging.getLogger('celery-logger')
 
 MAX_CONCURRENCY = 3
 MAX_RETRY = 3
+
+
+class PodcastHandler(Request):
+    def on_failure(self, info, send_failed=True, return_ok=False):
+        if type(info.exception) != Retry:
+            error_name = type(info.exception).__name__
+            podcast_id = self.kwargs["podcast_id"]
+            logger.critical(f'Failed to update podcast: id={podcast_id}: "{error_name}')
+        return super().on_failure(
+            info,
+            send_failed=send_failed,
+            return_ok=return_ok
+        )
