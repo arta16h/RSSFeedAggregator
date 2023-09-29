16a6a7247e47682c4b948f43af33e1f2b0acfc32
arta16h <sevda.hayati2015@gmail.com>
2023-09-29 07:30:13 +0330

    Chore: podcast recommendation url

--- podcasts/urls.py
+++ podcasts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PodcastListView, PodcastDetailView, EpisodeListView, EpisodeDetailView
+from .views import PodcastListView, PodcastDetailView, EpisodeListView, EpisodeDetailView, PodcastRecommendationAPIView
 
 urlpatterns = [
     path("podcasts/", PodcastListView.as_view(), name="podcast-list-create"),
     path("podcasts/<int:pk>/", PodcastDetailView.as_view(), name="podcast-detail"),
     path("podcasts/<int:pk>/episodes/", EpisodeListView.as_view(), name="podcast-episodes-list-create"),
     path("episodes/<int:pk>/", EpisodeDetailView.as_view(), name="episode-detail"),
+    path("recommends/", PodcastRecommendationAPIView.as_view(), name="recommends"),
 ]
