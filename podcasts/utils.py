f2a5e4cf546cc670925e7d26e60e492eb6a82983
arta16h <sevda.hayati2015@gmail.com>
2023-09-29 07:42:16 +0330

    Chore: like based recommended podcasts

--- podcasts/utils.py
+++ podcasts/utils.py
@@ -17,7 +17,15 @@ def subscription_based_recommended_podcasts(user):
     all_podcasts = Podcast.objects.all()
     user_podcasts = all_podcasts.filter(subscribe__user=user)
     sorted_categories = count_categories(user_podcasts)
-    podcasts = all_podcasts.exclude(Q(subscribe__user=user) | Q(main_fields__category=None)
-                    ).filter(main_fields__category__in=sorted_categories.keys())
+    podcasts = all_podcasts.exclude(Q(subscribe__user=user)).filter(main_fields__category__in=sorted_categories.keys())
+    podcasts_ids = list(podcasts.values_list("id", flat=True))
+    return podcasts_ids
+
+
+def like_based_recomended_podcasts(user):
+    all_podcasts = Podcast.objects.all()
+    user_liked_podcasts = all_podcasts.filter(podcast__like__user=user)
+    sorted_categories = count_categories(user_liked_podcasts)
+    podcasts = all_podcasts.exclude(Q(subscribe__user=user)).filter(main_fields__category__in=sorted_categories)
     podcasts_ids = list(podcasts.values_list("id", flat=True))
     return podcasts_ids
