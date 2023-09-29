a7c2700fb7c31e90e52d324f789f7aaf8cf694cd
arta16h <sevda.hayati2015@gmail.com>
2023-09-29 08:43:56 +0330

    Chore: interactions tasks.py

--- interactions/tasks.py
+++ interactions/tasks.py
@@ -4,10 +4,7 @@ from .models import Comment
 
 class BaseTask(Task):
     autoretry_for = (Exception,)
-    retry_kwargs = {"max_retries": 5}
+    max_retries = 5
     retry_backoff = True
     retry_jitter = False
-    task_acks_late = True
-    task_concurrency = 4
-    worker_prefetch_multiplier = 1
 
