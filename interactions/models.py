dbd44f181f9c5827b0715e15eb83eb2025dc8734
arta16h <sevda.hayati2015@gmail.com>
2023-10-01 19:15:29 +0330

    Refactor: bookmark model fields

--- interactions/models.py
+++ interactions/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.conf import settings
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.fields import GenericForeignKey
 
 from users.models import User
 from podcasts.models import Podcast, Episode
@@ -56,6 +53,7 @@ class Playlist:
 
 class Bookmark(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    episode_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey()
+    episode = models.ForeignKey(Episode, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.user
