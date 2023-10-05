e338081cd79b6145a43793f04cb91711be4fb913
arta16h <sevda.hayati2015@gmail.com>
2023-10-05 15:54:44 +0330

    Refactor: bind true for create comment

--- interactions/tasks.py
+++ interactions/tasks.py
@@ -1,14 +1,15 @@
 from celery import shared_task, Task
-from .models import Comment, Episode
+from .models import Comment
 
 
 class BaseTask(Task):
     autoretry_for = (Exception,)
     max_retries = 5
+    task_time_limit=120
     retry_backoff = True
     retry_jitter = False
 
-@shared_task(base=BaseTask, task_time_limit=120)
-def create_comment(content, episode_id, user_id):
+@shared_task(bind=True, base=BaseTask)
+def create_comment(self, content, episode_id, user_id):
     Comment.objects.create(content=content, object_id=episode_id, user_id=user_id)
     return f"New comment submitted by {user_id}"
