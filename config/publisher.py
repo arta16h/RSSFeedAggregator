1470d064a2e53524f5421835e8457fc226e99668
arta16h <sevda.hayati2015@gmail.com>
2023-10-27 20:57:22 +0330

    Chore: publisher

--- config/publisher.py
+++ config/publisher.py
@@ -1,1 +1,12 @@
 import pika
+
+class Publisher:
+    def __init__(self) :
+        self.connection_parameter = pika.ConnectionParameters('localhost')
+        self.connection = pika.BlockingConnection(self.connection_parameter)
+        self.channel = self.connection.channel()
+        self.channel.queue_declare('signup-login')
+
+    def publish(self, message, queue) :
+        self.channel.basic_publish(exchange='', routing_key=queue, body=message)
+        print(f'{message} Published Successfully!')
