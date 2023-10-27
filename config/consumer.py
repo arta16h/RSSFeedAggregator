86c3f58c445f9699b5d551d01c596d7c4e349a59
arta16h <sevda.hayati2015@gmail.com>
2023-10-27 21:21:52 +0330

    Chore: consumer

--- config/consumer.py
+++ config/consumer.py
@@ -1,9 +1,15 @@
 import pika
+import logging
 
 connection_parameter = pika.ConnectionParameters('localhost')
 connection = pika.BlockingConnection(connection_parameter)
 channel = connection.channel()
 channel.queue_declare(queue='signup-login')
 
+logger = logging.getLogger('user_actions')
+
 def log_user_activity(ch, method, property, body) :
-    
+    logger.info(body)
+
+channel.basic_consume(queue='signup-login', on_message_callback=log_user_activity, auto_ack=True)
+print('Start Consuming...')
