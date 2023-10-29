257922dfad5eeebb1e67449318a5797d93e898f3
arta16h <sevda.hayati2015@gmail.com>
2023-10-29 20:58:05 +0330

    Chore: log sender

--- config/elk.py
+++ config/elk.py
@@ -1,6 +1,23 @@
 from logging import Handler
-import elasticsearch, elasticsearch_dsl
+import datetime
+import time
+import json
+
 from elasticsearch import Elasticsearch
+
+
+class LogSender:
+    def __init__(self,elk) -> None:
+        self.elk = elk
+
+    def writelog(self, message, formatter) :
+        index_name = f'log_{time.strftime("%Y_%m_%d")}'
+        timestamp = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+        log_data = json.loads(formatter(message))
+        log_data['timestamp'] = timestamp
+        log_data['level'] = message.levelname
+        self.elk.index(index=index_name, document=log_data)
 
 
 class ElkHandler(Handler) :
@@ -9,13 +26,11 @@ class ElkHandler(Handler) :
         self.host = host
         self.formatter = kwargs.get('formatter')
         self.elk = Elasticsearch(hosts=self.host)
+        self.sender = LogSender(elk=self.elk)
 
     def emit(self, record) :
         try:
-            msg = self.format(record)
-            index = self.elk.index(index='')
-            index.write(msg + self.terminator)
-            self.flush()
+            self.sender.writelog(record, self.formatter)
         except RecursionError:
             raise
         except Exception:
