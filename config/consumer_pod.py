d20da4c924a0cb051f9cc71c2631cf15fa10553b
arta16h <sevda.hayati2015@gmail.com>
2023-10-30 17:30:23 +0330

    Chore: PodcastActivityConsumer

--- config/consumer_pod.py
+++ config/consumer_pod.py
@@ -1,1 +1,27 @@
 import pika
+import logging
+
+class PodcastActivityConsumer :
+    def __init__(self) -> None:
+        self.logger = logging.getLogger('api_logger')
+        self.connection_parameter = pika.ConnectionParameters('localhost')
+        self.connection = pika.BlockingConnection(self.connection_parameter)
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='podcast-update')
+
+
+    def consume(self):
+        self.channel.basic_consume(queue='podcast-update', on_message_callback=self.log_pod_activity, auto_ack=True)
+        print('Start Consuming...')
+        self.channel.start_consuming()
+
+    def log_pod_activity(self, channel, method, property, body) :
+        body = body.decode("utf-8")
+        if body.startswith('success') :    
+            self.logger.info(body.lstrip("success"))
+            print(body)
+        elif body.startswith('error!!') :
+            self.logger.error(body.lstrip("error!!"))
+            print(body)
+        else :
+            print("Error!")
