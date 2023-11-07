5b6c5fcae02780dd3f5a4efcfa7ebf7a045389d7
arta16h <sevda.hayati2015@gmail.com>
2023-11-07 23:54:27 +0330

    Chore: add daily index to logs

--- config/elk.py
+++ config/elk.py
@@ -10,26 +10,28 @@ class LogSender:
     def __init__(self, elk) -> None:
         self.elk = elk
 
-    def writelog(self, message, formatter) :
-        index_name = f'log_{time.strftime("%Y_%m_%d")}'
+    def writelog(self, message, formatter, db_name=None, daily_index=False) :
+        suffix = "_" + str(time.strftime("%Y_%m_%d")) if daily_index else ""
+        index_name = db_name + suffix or f'log_{time.strftime("%Y_%m_%d")}'
         timestamp = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
         log_data = {'message' : formatter(message)}
         log_data['timestamp'] = timestamp
         log_data['level'] = message.levelname
-        self.elk.index(index=index_name, document=log_data)
+        self.elk.index(index=index_name.lower(), document=log_data)
         
 
-class ElkHandler(Handler) :
-    def __init__(self, host, *args, **kwargs) :
+class ElasticHandler(Handler) :
+    def __init__(self, host,*args, **kwargs) :
+        self.db_name = kwargs.pop('db_name', None)
+        self.daily_index = kwargs.pop('daily_index', False)
         super().__init__(*args, **kwargs)
         self.host = host
-        # self.formatter = kwargs.get('formatter')
-        self.elk = Elasticsearch(hosts=self.host)
+        self.elk = Elasticsearch(self.host)
         self.sender = LogSender(elk=self.elk)
 
     def emit(self, record) :
         try:
-            self.sender.writelog(record, self.format)
+            self.sender.writelog(message=record, formatter=self.format, db_name=self.db_name, daily_index=self.daily_index)
         except RecursionError:
             raise
         except Exception:
